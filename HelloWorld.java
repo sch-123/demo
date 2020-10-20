5cf75b3e73ac91718d5c1842fd64102fbdf55e48
sch-123 <2473276269@qq.com>
2020-10-20 11:18:02 +0800

    helloworld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+   public static void mian(String[] args){
+           System.out.println("HelloWorld");
+	System.out.println("3");
+	System.out.println("HelloWorld");
+	System.out.println("4");
+	}
+}
